2e408717c873f5ff2615fdbbd02a907b7f971079
tandn164 <kenjikatoji999@gmail.com>
2021-06-09 17:52:26 +0700

    add admin dashboard

---
+++ includes/banner.php
@@ -0,0 +1,27 @@
+<section class="site-section pt-5 pb-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+
+                <div class="owl-carousel owl-theme home-slider">
+                    <div>
+                        <a href="" class="a-block d-flex align-items-center height-lg"
+                           style="background-image: url('images/banner.jpg'); ">
+
+                        </a>
+                    </div>
+                    <div>
+                        <a href="" class="a-block d-flex align-items-center height-lg"
+                           style="background-image: url('images/banner_work.png'); ">
+
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+
+</section>
---
+++ includes/footer.php
@@ -0,0 +1,56 @@
+<style>
+    center {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 50%;
+    }
+</style>
+
+<footer class="site-footer" style="padding: 10px">
+    <div class="container">
+        <div class=" col md-12 row" style="text-align: center">
+            <a href="https://www.facebook.com/hoangduc.anh.1420/" class="col-md-3 center">
+                <img src="images/facebook.png" alt="Image placeholder" class="img-fluid" style="width: 100px">
+            </a>
+            <a href="https://twitter.com/HongcAn52449307" class="col-md-3 center">
+                <img src="images/twitter.png" alt="Image placeholder" class="img-fluid" style="width: 100px">
+            </a>
+            <a href="https://www.linkedin.com/in/hoang-duc-anh-668872192/" class="col-md-3 center">
+                <img src="images/linkedin.png" alt="Image placeholder" class="img-fluid" style="width: 100px">
+            </a>
+            <a href="https://github.com/ducanhlhp9" class="col-md-3 center">
+                <img src="images/github.png" alt="Image placeholder" class="img-fluid" style="width: 100px">
+            </a>
+        </div>
+
+
+    </div>
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <p class="small">
+                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                Copyright &copy;
+                <script data-cfasync="false"
+                        src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+                <script>document.write(new Date().getFullYear());</script>
+                All Rights Reserved | This template is made with <i class="fa fa-heart text-danger"
+                                                                    aria-hidden="true"></i> by <a
+                        href="https://colorlib.com" target="_blank">Colorlib</a>
+                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            </p>
+        </div>
+</footer>
+
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="js/jquery-migrate-3.0.0.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.waypoints.min.js"></script>
+<script src="js/jquery.stellar.min.js"></script>
+
+
+<script src="js/main.js"></script>
+</body>
+</html>
